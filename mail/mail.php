d01b4014a8f361304dc8a8788f55c888de7fd861
tegaesi <66805431+tegaesi@users.noreply.github.com>
2021-09-20 22:57:58 +0100

    Update mail.php
    Trying to get the mail on the site to work

--- mail/mail.php
+++ mail/mail.php
@@ -3,16 +3,12 @@ $name = $_POST{'name'};
 $email = $_POST{'email'};
 $message = $_POST['message'];
 
-$email_message = "
+$email_message = "";
 
-Name: ".$name."
-Email: ".$email."
-Message: ".$message."
-
-";
+Name: ".$name.";
+Email: ".$email.";
+Message: ".$message.";
 
 mail ("esitega@yahoo.com" , "New Message", $email_message);
 header("tegaesi.github.io/mail-success.html");
 ?>
-
-
